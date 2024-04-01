032ee4ef505b7e9d44a8d5edcb68611112164c96
tara <t.dinreinwald512@mybvc.ca>
2024-03-31 19:36:40 -0600

    Music Working
    just need to do sound affects

---
+++ Assets/Scripts/Audio/InGameAudio.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameAudio : MonoBehaviour
+{
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play(); // Play the audio on start
+    }
+
+    public void ToggleMute()
+    {
+        audioSource.mute = !audioSource.mute; // Toggle mute state
+    }
+}
