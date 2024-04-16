8897d1d60c6698aed403b1ee25fe3e7f96419317
tara <t.dinreinwald512@mybvc.ca>
2024-04-16 13:25:42 -0600

    fixing Ui Pause menu

---
+++ Assets/Scripts/UI/Pause.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    // Start is called before the first frame update
+    
+    public void Stop()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void Home()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void Reset()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
