24778a08457fdb39d0c1e1ad5e935a8e2b01759b
tara <t.dinreinwald512@mybvc.ca>
2024-04-19 00:19:42 -0600

    main Screen ui changes

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -2,31 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 
 public class MainMenu : MonoBehaviour
 {
-    private bool _controlsOpen = false;
+    
 
-    [SerializeField]
-    private GameObject _controls;
-
-    [SerializeField]
-    private GameObject _startButton;
-
-    [SerializeField]
-    private GameObject _closeButton;
-
-    private void Awake()
-    {
-        _controls = GameObject.Find("Controls"); // Finds the Controls Button
-
-        _startButton = GameObject.Find("ResumeButton"); // Finds the Start Button
-
-        _closeButton = GameObject.Find("CloseButton"); // Finds the Start Button
-    }
+    
     void Start()
     {
   
